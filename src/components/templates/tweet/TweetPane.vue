40579d010f8d7b2a186dab6b3d3a2702775ade47
guillem <guillem@disedit.com>
2020-06-10 13:16:38 +0200

    Style tweet pane

--- src/components/templates/tweet/TweetPane.vue
+++ src/components/templates/tweet/TweetPane.vue
@@ -9,29 +9,48 @@
       <b-input
         v-model="properties.tweetUrl"
         placeholder="https://"
-        :disabled="fetching"
+        :disabled="fetching || properties.tweetId"
         @keydown.native="(e) => handleKeyStrokes(e)">
       </b-input>
     </b-field>
-    <div v-if="fetching">Carregant tweet...</div>
-    <div v-if="properties.tweetEmbed">
-      <ul>
-        <li>{{ properties.tweetEmbed.user.name }}</li>
-        <li>@{{ properties.tweetEmbed.user.screen_name }}</li>
-        <li>{{ properties.tweetEmbed.full_text }}</li>
-      </ul>
+    <div v-if="fetching" class="fetching">Carregant tweet...</div>
+
+    <!-- Tweet card -->
+    <div v-if="properties.tweetEmbed" class="tweet-embed mini-card">
+      <div class="tweet-embed-pic"><img :src="properties.tweetEmbed.user.profile_image_url_https" /></div>
+      <div class="tweet-embed-user">
+        <strong>{{ properties.tweetEmbed.user.name }}</strong>
+        <div>@{{ properties.tweetEmbed.user.screen_name }}</div>
+      </div>
+      <div class="tweet-embed-text">{{ properties.tweetEmbed.full_text }}</div>
+      <div class="tweet-embed-remove">
+        <b-button
+          @click="clearTweet"
+          class="remove-image"
+          type="is-danger"
+          size="is-small"
+          icon-right="times">
+        </b-button>
+      </div>
     </div>
 
     <!-- Tweet media -->
     <transition name="slide">
       <div v-if="properties.tweetEmbed && properties.tweetEmbed.entities['media']">
-        <b-switch v-model="properties.showMedia">
+        <b-switch v-model="properties.showMedia" style="margin-bottom: 1rem">
           Mostra imatge del tweet
         </b-switch>
 
-        <!-- Picture position -->
         <transition name="slide">
           <div v-if="properties.showMedia">
+            <!-- Pictures -->
+            <div class="twitter-media mini-card">
+              <div v-for="(media, i) in properties.tweetEmbed.entities.media" :key="i" class="twitter-media-item">
+                <img :src="media.media_url_https" />
+              </div>
+            </div>
+
+            <!-- Picture position -->
             <b-field label="Posició de la imatge" class="range">
               <range-slider
                 name="points"
@@ -111,11 +130,11 @@ export default {
       const matched = url.match(/(^|[^'"])(https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+))/)
       const id = (matched) ? matched[4] : false
 
-      if (!id) {
+      if (!id && url !== '') {
         this.displayErrors = true
         this.errors.tweetId = ['La URL del tweet sembla no ser vàlida']
         return false
-      } else if (id !== this.properties.tweetId) {
+      } else if (id !== this.properties.tweetId && url) {
         this.fetching = true
         this.properties.tweetId = null
         this.properties.tweetEmbed = null
@@ -141,13 +160,85 @@ export default {
         this.properties.tweetId = ''
         this.properties.tweetEmbed = ''
       }
+    },
+
+    clearTweet () {
+      this.properties.tweetUrl = ''
+      this.properties.tweetId = null
+      this.properties.tweetEmbed = null
+      this.displayErrors = false
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import "../../../sass/variables";
+
   .local-label {
     margin-top: .75rem;
+  }
+
+  .fetching {
+    margin-top: -.75rem;
+    margin-bottom: 1rem;
+    color: $gray-700;
+    font-size: .85rem;
+  }
+
+  .mini-card {
+    border: 2px $gray-200 solid;
+    padding: 1rem;
+    font-size: .85rem;
+    border-radius: .5rem;
+    margin-bottom: 1rem;
+  }
+
+  .tweet-embed {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    grid-template-rows: auto auto auto;
+    grid-template-areas:
+      "pic user remove"
+      "text text text";
+    gap: .25rem;
+
+    &-pic {
+      grid-area: pic;
+
+      img {
+        width: 35px;
+        border-radius: 50%;
+      }
+    }
+
+    &-user {
+      grid-area: user;
+      align-self: center;
+      line-height: 1.25;
+      margin-left: .25rem;
+
+      & > div {
+        color: $gray-700;
+      }
+    }
+
+    &-text {
+      grid-area: text;
+    }
+
+    &-remove {
+      grid-area: remove;
+    }
+  }
+
+  .twitter-media {
+    img {
+      display: block;
+      width: 60px;
+      height: 60px;
+      object-fit: cover;
+      border-radius: .5rem;
+    }
   }
 </style>
