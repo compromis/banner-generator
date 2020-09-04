c45521cc898429952bf14a127a196ddd128ff8f5
guillem <guillem@disedit.com>
2020-09-04 10:22:17 +0200

    Add glowy-card component props

---
+++ src/utils/GlowyCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="glowy-card">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'glowy-card',
+
+  props: {
+    image: {
+      type: File,
+      default: () => null
+    },
+    gradient: {
+      type: String,
+      default: 'orange',
+      validator: function (value) {
+        return ['orange', 'feminism', 'green', 'black'].indexOf(value) !== -1
+      }
+    },
+    edge: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
