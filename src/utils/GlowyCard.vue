f3b7d56c3b2f852fc03f5184e4fa95bd2656d9cf
guillem <guillem@disedit.com>
2020-09-04 11:27:59 +0200

    Add glowy-card component functionality

--- src/utils/GlowyCard.vue
+++ src/utils/GlowyCard.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="glowy-card">
-
+  <div :class="['glowy-card', `gradient-${gradient}`, {'edge': edge}]">
+    <div class="glowy-subject">
+      <img v-if="image" :src="image" :style="imagePosition" />
+      <div v-else class="placeholder"></div>
+    </div>
+    <div class="glowy-ghost first">
+      <img v-if="image" :src="image" :style="imagePosition" />
+      <div v-else class="placeholder"></div>
+    </div>
+    <div class="glowy-ghost second">
+      <img v-if="image" :src="image" :style="imagePosition" />
+      <div v-else class="placeholder"></div>
+    </div>
   </div>
 </template>
 
@@ -10,14 +21,18 @@ export default {
 
   props: {
     image: {
-      type: File,
-      default: () => null
+      type: [File, String],
+      default: null
+    },
+    imagePosition: {
+      type: Object,
+      default: null
     },
     gradient: {
       type: String,
       default: 'orange',
       validator: function (value) {
-        return ['orange', 'feminism', 'green', 'black'].indexOf(value) !== -1
+        return ['orange', 'feminism', 'green', 'none'].indexOf(value) !== -1
       }
     },
     edge: {
@@ -29,5 +44,67 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '../sass/variables';
 
+  .glowy {
+    &-card {
+      position: relative;
+    }
+
+    &-subject {
+      position: relative;
+      z-index: 2;
+    }
+
+    &-ghost {
+      position: absolute;
+      top: 5px;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 1;
+      filter: blur(34px);
+
+      &.second {
+        filter: blur(8px);
+      }
+    }
+
+    &-subject,
+    &-ghost {
+      border-radius: $glowy-card-radius;
+      overflow: hidden;
+
+      img {
+        display: block;
+        margin: 0;
+      }
+
+      @at-root .edge > div {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+
+  .gradient {
+    @each $name, $gradient in $trans-gradients {
+      &-#{$name} {
+        .glowy-subject::before,
+        .glowy-ghost::before {
+          content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background: $gradient;
+        }
+      }
+    }
+  }
 </style>
