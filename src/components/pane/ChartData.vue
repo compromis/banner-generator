c52d0a0f7ef4e0ff9eeddd987ecb775d14588816
Guillem <guillem@disedit.com>
2021-01-19 14:31:36 +0100

    Add basic chart input form

--- src/components/pane/ChartData.vue
+++ src/components/pane/ChartData.vue
@@ -1,12 +1,42 @@
 <template>
   <div>
-    Chart data input
+    <ul>
+      <li v-for="(set, setKey) in chartdata.datasets" :key="setKey">
+        <input type="text" v-model="set.label" />
+        <input type="text" v-model="set.backgroundColor" />
+        <table>
+          <tr>
+            <th>Etiqueta</th>
+            <th>Valor</th>
+          </tr>
+          <tr v-for="(datapoint, i) in set.data" :key="set.label + i">
+            <td><input type="text" v-model="chartdata.labels[i]" /></td>
+            <td><input type="text" v-model="set.data[i]" /></td>
+          </tr>
+        </table>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'chart-data'
+  name: 'chart-data',
+
+  data () {
+    return {
+      chartdata: {
+        labels: ['January', 'February'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20]
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
