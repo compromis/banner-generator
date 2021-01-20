abcbc096b054b36ee3d79ac2a9449864d52e1c06
Carles <carles2612@gmail.com>
2021-01-20 14:03:32 +0100

    Style BarChart

--- src/components/canvas/BarChartCss.vue
+++ src/components/canvas/BarChartCss.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>{{ message }}</h1>
-    <div class="chart">
+    <div :class="{'chart' : true, 'chart-horizontal' : horizontal}">
       <div class="chart-item" v-for="(data, d) in chart.data" :key="d">
         <div class="chart-item-bars">
           <div
@@ -9,10 +9,13 @@
             :key="set"
             class="chart-item-bar"
             :style="{
-              height: calcHeight(value.number, 200),
-              backgroundColor: value.color === 'inherit' ? chart.sets[set].color : value.color
+              width: horizontal ? calcLength(value.number) : 'reset',
+              height: horizontal ?  'reset' : calcLength(value.number),
+              backgroundColor: value.color
             }"
-          ></div>
+          >
+            <div :style="{color: value.highlight ? value.color : null}" :class="{'chart-item-number' : true, 'chart-item-number-starred' : value.highlight}">{{ value.number }}</div>
+          </div>
         </div>
         <div class="chart-item-label">{{ data.label }}</div>
       </div>
@@ -26,6 +29,14 @@ export default {
     chart: {
       type: Object,
       required: true
+    },
+    horizontal: {
+      type: Boolean,
+      default: true
+    },
+    maxLength: {
+      type: Number,
+      default: 400
     }
   },
 
@@ -36,17 +47,18 @@ export default {
   },
 
   methods: {
-    calcHeight (value, maxHeight) {
-      const height = value * maxHeight / this.heightestValue
-      return height + 'px'
+    calcLength (value) {
+      const length = value * this.maxLength / this.highestValue
+      return length + 'px'
     }
   },
 
   computed: {
-    heightestValue () {
+    highestValue () {
       let values = []
       this.chart.data.forEach(row => {
-        values = [...values, ...row.values]
+        const rowValues = row.values.map(value => value.number)
+        values = [...values, ...rowValues]
       })
 
       return Math.max(...values)
@@ -63,30 +75,103 @@ export default {
   grid-template-rows: 1fr;
   grid-auto-flow: column;
   align-items: end;
+  justify-items: center;
+  gap: 18px;
 }
 
 .chart-item {
   display: flex;
   align-items: center;
   flex-direction: column;
+  width: 100%;
 
   &-label {
     color: $gray-700;
-    font-size: 18px;
+    font-size: 16px;
+    margin-top: 8px;
+    flex-shrink: 0;
+    flex-grow: 0;
+    height: 50px;
+    text-align: center;
+    line-height: 1.2;
+    overflow: hidden;
   }
 
   &-bars {
     display: flex;
     align-items: flex-end;
+    justify-content: center;
+    width: 100%;
   }
 
   &-bar {
+    position: relative;
     margin-bottom: .5rem;
     background-color: $orange;
-    width: 30px;
+    width: 100%;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
-    margin: 0 5px;
+    margin: 0 5%;
+    max-width: 60px;
   }
+
+  &-number {
+    position: absolute;
+    font-size: 16px;
+    color: $gray-700;
+    top: -30px;
+    left: 50%;
+    transform: translate(-50%, 0);
+
+    &-starred {
+      top: -40px;
+      font-size: 24px;
+      color: red;
+    }
+  }
+}
+
+.chart-horizontal {
+  grid-auto-flow: row;
+  align-items: start;
+
+  .chart-item {
+    flex-direction: row-reverse;
+
+  &-label {
+    text-align: left;
+    width: 130px;
+    margin-top: 0;
+    margin-right: 8px;
+    height: auto;
+  }
+
+  &-bars {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    width: 100%;
+    flex-direction: column;
+  }
+
+  &-bar {
+    margin: .5% 0;
+    max-width: 100%;
+    min-height: 25px;
+    border-top-left-radius: 0;
+    border-bottom-right-radius: 10px;
+  }
+
+  &-number {
+    left: unset;
+    right: -40px;
+    top: 50%;
+    transform: translate(0, -50%);
+
+    &-starred {
+      right: -50px;
+    }
+  }
+}
 }
 </style>
