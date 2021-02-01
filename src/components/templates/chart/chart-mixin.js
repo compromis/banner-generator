ad107396385d7190e3558255e431870523a3a2c3
Guillem <guillem@disedit.com>
2021-02-01 12:03:32 +0100

    Increase maximumFractionDigits in charts

--- src/components/templates/chart/chart-mixin.js
+++ src/components/templates/chart/chart-mixin.js
@@ -1,14 +1,13 @@
 export default {
   methods: {
     formatNumber (number) {
-      const options = { notation: 'compact', compactDisplay: 'short' }
+      const options = { notation: 'compact', compactDisplay: 'short', maximumFractionDigits: 4 }
       if (this.chart.options.valuesIn === 'euros') {
         options.style = 'currency'
         options.currency = 'EUR'
       } else if (this.chart.options.valuesIn === 'percentage') {
         number = number / 100
         options.style = 'percent'
-        options.maximumFractionDigits = 0
       }
 
       return new Intl.NumberFormat('es-ES', options).format(number)
