134119e2d9bdcbb39c3991835059e2c231a7951c
lecarles <carles2612@gmail.com>
2021-03-02 14:09:33 +0100

    Add empty card structure

--- src/components/templates/cards/CardsCanvas.vue
+++ src/components/templates/cards/CardsCanvas.vue
@@ -9,6 +9,11 @@
       'logo-' + banner.logo
     ]"
     v-if="banner">
+    <div class="card-grid">
+      <div class="card" v-for="(card, i) in banner.cards" :key="i" :class="['card-colspan-' + card.colspan, 'card-rowspan-' + card.rowspan]">
+        {{i}}
+      </div>
+    </div>
     <emojis-on-canvas v-model="banner.emojis" />
     <banner-frame />
     <banner-picture :picture-position="objectPosition" />
@@ -36,5 +41,39 @@ export default {
 
 <style lang="scss" scoped>
   @import "../../../sass/variables";
+
+  .card-grid {
+    margin: 32px;
+    height: 590px;
+    gap: 12px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-auto-flow: dense;
+
+    .card {
+      z-index: 10;
+
+      &-colspan {
+        &-2 {
+          grid-column: span 2;
+        }
+
+        &-3 {
+          grid-column: span 3;
+        }
+      }
+
+      &-rowspan {
+        &-2 {
+          grid-row: span 2;
+        }
+
+        &-3 {
+          grid-row: span 3;
+        }
+      }
+    }
+  }
 
 </style>
