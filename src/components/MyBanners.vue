2c061205ffc83fda4fa81f8206a883f3314c0f23
lecarles <carles2612@gmail.com>
2021-04-08 12:29:21 +0200

    Add MyBanner page

---
+++ src/components/MyBanners.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="my-banners">
+    <h1>Les meues targes</h1>
+    <div class="my-banners-list">
+      <banner-item />
+    </div>
+  </div>
+</template>
+
+<script>
+import BannerItem from '@/components/ui/BannerItem'
+
+export default {
+  name: 'my-banners',
+
+  components: {
+    BannerItem
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "../sass/variables";
+
+  .my-banners {
+    display: grid;
+    grid-template-rows: 1fr auto;
+    padding-top: 5.25rem;
+    margin: 0 auto;
+    position: relative;
+    max-width: 1000px;
+
+    h1 {
+      font-size: 2rem;
+      color: $gray-700;
+    }
+  }
+</style>
---
+++ src/components/ui/BannerItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="banner-item">
+    <div class="banner-item-thumbnail">
+
+    </div>
+    <div class="banner-item-info">
+      <div class="banner-title"></div>
+      <div class="banner-type"></div>
+      <div class="banner-saved"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'banner-item'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../sass/variables";
+.banner-item {
+  &-thumbnail {
+    background: $gray-400;
+    border-radius: 30px;
+    height: 180px;
+    width: 180px;
+  }
+}
+</style>
