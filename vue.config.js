7ad54e4ab86279c4efd3d0b916dacf1b5968c130
Guillem <guillem@disedit.com>
2021-06-16 11:19:20 +0200

    Add local env

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,8 @@
-const ImageminPlugin = require('imagemin-webpack-plugin').default
-
 module.exports = {
   configureWebpack: () => {
     if (process.env.NODE_ENV === 'production') {
       return {
-        devtool: 'source-map',
-        plugins: [
-          new ImageminPlugin()
-        ]
+        devtool: 'source-map'
       }
     }
   }
