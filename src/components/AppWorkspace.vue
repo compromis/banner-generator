57ac5a3aa6dfb47ce828dca95f1cda3b313e5980
lecarles <carles2612@gmail.com>
2021-07-07 13:14:49 +0200

    Add login reminder popup

--- src/components/AppWorkspace.vue
+++ src/components/AppWorkspace.vue
@@ -7,6 +7,13 @@
       <component :is="template.components.pane" class="pane" />
       <canvas-container :canvas-component="template.components.canvas" class="canvas" />
       <help id="help-button" class="help-block" />
+      <c-popup :showPopup="showPopup">
+        Inicia sessió amb l'espai compromís per a guardar les teues targes.
+        <template v-slot:buttons>
+          <b-button @click="hideReminder">Ara no</b-button>
+          <b-button type="is-primary" class="mr-0">Incia sessió</b-button>
+        </template>
+      </c-popup>
 
       <portal to="confirm">
         <b-modal :active="isCardModalActive" @close="isCardModalActive = false" :width="640" scroll="keep">
@@ -39,6 +46,7 @@ import CanvasContainer from './CanvasContainer'
 import Help from './Help'
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
+import CPopup from './ui/CPopup'
 
 export default {
   name: 'app-workspace',
@@ -46,7 +54,8 @@ export default {
   components: {
     CanvasContainer,
     Help,
-    Loading
+    Loading,
+    CPopup
   },
 
   data () {
@@ -55,7 +64,8 @@ export default {
       goingTo: null,
       banner: null,
       error: false,
-      ssoLoginUrl: process.env.VUE_APP_SSO_LOGIN_URL
+      ssoLoginUrl: process.env.VUE_APP_SSO_LOGIN_URL,
+      showPopup: false
     }
   },
 
@@ -85,6 +95,10 @@ export default {
     } catch (error) {
       this.error = true
     }
+    const showReminder = localStorage.getItem('login_reminder')
+    if (!this.isLoggedIn && showReminder !== 'hidden') {
+      this.showPopup = true
+    }
   },
 
   watch: {
@@ -110,6 +124,11 @@ export default {
       this.$store.commit('setTemplate', template)
       this.$store.commit('setAspect', template.aspects[0])
       this.$store.commit('setDisplayErrors', false)
+    },
+
+    hideReminder () {
+      this.showPopup = false
+      localStorage.setItem('login_reminder', 'hidden')
     }
   },
 
---
+++ src/components/ui/CPopup.vue
@@ -0,0 +1,54 @@
+<template>
+  <transition name="slide-fade">
+    <div class="card popup-card" v-if="showPopup">
+      <div class="info"><slot></slot></div>
+      <div class="popup-buttons"><slot name="buttons"></slot></div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    showPopup: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../sass/variables";
+
+.popup-card {
+  position: absolute;
+  padding: 1rem;
+  top: .75rem;
+  right: .75rem;
+  width: 100%;
+  max-width: 325px;
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
+
+  .info {
+    font-size: 1.15rem;
+    margin-bottom: 1.75rem;
+  }
+
+  .popup-buttons {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+
+@media (max-width: $xs-breakpoint) {
+  .popup-card {
+      position: fixed;
+      top: 4.75rem;
+      left: 50%;
+      transform: translateX(-50%);
+  }
+}
+</style>
