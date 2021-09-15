2a6341e8aedadbcd9b98ae5a9c51a3bf11b468bd
Guillem <guillem@disedit.com>
2021-09-15 09:43:41 +0200

    Do not save image blob url to db

--- src/mixins/pane-mixin.js
+++ src/mixins/pane-mixin.js
@@ -106,7 +106,8 @@ export default {
   methods: {
     save: debounce(async (self) => {
       const { banner, bannerMeta } = self.$store.state
-      const updated = await http.update(bannerMeta.ref, JSON.stringify(banner))
+      const { pictureBlob, ...bannerToSave } = banner
+      const updated = await http.update(bannerMeta.ref, JSON.stringify(bannerToSave))
       self.$store.commit('setBannerMeta', updated)
     }, 1000),
 
